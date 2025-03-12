610c9b98faf7ddb4ee8298afd91d94ac766ce13c
Guillermo Villanueva <guillermo@benchpro.com>
2025-03-11 19:25:19 -0700

    Initial Commit

---
+++ WebsiteAppTests/WebsiteAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebsiteAppTests.swift
+//  WebsiteAppTests
+//
+//  Created by GUILLERMO VILLANUEVA on 3/11/25.
+//
+
+import Testing
+@testable import WebsiteApp
+
+struct WebsiteAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
